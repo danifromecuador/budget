9f07ebd1f5c9f072e38260e7245793ee6f1280a2
dani <danifromecuador@gmail.com>
2023-10-24 18:50:59 -0500

    Run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -26,12 +26,12 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_24_182658) do
   create_table "expenses", force: :cascade do |t|
     t.string "name"
     t.decimal "amount"
-    t.bigint "user_id", null: false
+    t.bigint "author_id", null: false
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_expenses_on_author_id"
     t.index ["category_id"], name: "index_expenses_on_category_id"
-    t.index ["user_id"], name: "index_expenses_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,5 +42,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_24_182658) do
 
   add_foreign_key "categories", "users"
   add_foreign_key "expenses", "categories"
-  add_foreign_key "expenses", "users"
+  add_foreign_key "expenses", "users", column: "author_id"
 end
