847e3618e6b9a20a4aed841e683872473a341a2d
dani <danifromecuador@gmail.com>
2023-10-24 22:29:23 -0500

    Edit the definitive relationships between models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
   belongs_to :user
+  has_many :expenses
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,4 @@
-class Expense < ApplicationRecord
-  belongs_to :user
+class Expense < ApplicationRecord  
+  belongs_to :author, class_name: 'User'
   belongs_to :category
 end
