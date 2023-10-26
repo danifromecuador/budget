ca31567604ce94e6ad10d12998d26859ad0e0b7f
dani <danifromecuador@gmail.com>
2023-10-26 14:06:46 -0500

    Fix rubocop linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
   end
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     categories_path
   end
-  
 end
