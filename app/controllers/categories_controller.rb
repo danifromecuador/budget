c6e0bd966ef22adbb543d4ca648f7bf5f2e8fe93
dani <danifromecuador@gmail.com>
2023-10-27 14:41:38 -0500

    Create categories new page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :authenticate_user!
+
   def index
     @categories = Category.all
   end
@@ -10,5 +11,23 @@ class CategoriesController < ApplicationController
 
   def new
     @category = Category.new
+    @icons = ['&#x1F600;', '&#x1F601;', '&#x1F602;', '&#x1F603;', '&#x1F604;', '&#x1F605;', '&#x1F606;', '&#x1F607;',
+              '&#x1F608;', '&#x1F609;']
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to categories_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon, :user_id)
   end
 end
