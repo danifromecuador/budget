81bc83b1be5e2051fc82610b41bc0ca4b4c33e15
dani <danifromecuador@gmail.com>
2023-10-27 13:52:25 -0500

    Create categories show page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,12 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def show
+    @expenses = Expense.where(category_id: params[:id])
+  end
+
+  def new
+    @category = Category.new
+  end
 end
