ce57c925a7087c2ec12d804c4d7d692fc4690d6a
dani <danifromecuador@gmail.com>
2024-07-10 17:41:49 -0500

    Update script to include database commands | deployment | bin/render-build.sh |

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,17 @@
 # exit on error
 set -o errexit
 
+# Install dependencies
 bundle install
+
+# Precompile assets
 bundle exec rake assets:precompile
+
+# Clean up assets
 bundle exec rake assets:clean
+
+# Drop, create, and migrate the database
+bundle exec rails db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+bundle exec rails db:create
+bundle exec rails db:schema:load
+bundle exec rails db:migrate
